d95c4c38e60449adf418fed2cc849cdc0f5e180b
Sleri <nagudelo@iegabo.edu.co>
2019-10-15 15:37:31 -0500

    Mensajes para cuando se edita y/o actualiza algun dato
    Se hizo la inserción de los mensajes correspondientes en cuanto a los medicos

--- Controlador/updateMedico.php
+++ Controlador/updateMedico.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Cl&iacute;nica Cotecnova</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+    <div class="col-lg-offset-3 col-lg-6">
 <?php
 //condicion para comprobar si los campos están declarados anteriormente y si no estan vacíos
 if(isset($_POST['enviar']) && !empty($_GET['id']) && !empty($_POST['nombreCompleto'])
@@ -23,16 +40,25 @@ if(isset($_POST['enviar']) && !empty($_GET['id']) && !empty($_POST['nombreComple
     
         //decision para comprobar si se ejecuto, se redirige al index principal
         if($actualizar){
-           header("Location: ../ver_medico.php");
+           echo "<div class=\"alert alert-success alert-dismissible\"><a href=\"../ver_usuario.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Felicidades!</strong>El medico ha sido actualizado correctamente.</div>";
+           header( "refresh:3;url=../ver_medico.php" ); 
         } else {
             //mensaje de error
-            echo "Error";
+            echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../ver_usuario.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se ha podido actualizar al medico.</div>";
+            header( "refresh:3;url=../ver_medico.php" );         
         }
     //Desconecto la conexion de la bD
     $mysql->desconectar(); 
     //header("Location: ../index.php");
     
 }else{
-    header("Location: ../index.php");
-    //sino se cumple la primer condicion, se re envia nuevamente al formulario
+    echo "<div class=\"alert alert-warning alert-dismissible\"><a href=\"../ver_usuario.php\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a><strong>Alerta!</strong>No se han enviado todos los datos necesarios.</div>";
+    header( "refresh:3;url=../ver_medico.php" );  
 }
+?>
+  </div>
+  <!-- Llamado de los respectivos scripts -->
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
