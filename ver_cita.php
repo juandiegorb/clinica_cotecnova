d18c7422476683eccf5adf35eefb39a7a8a93ac8
juandiegorb <juan.drb.99@gmail.com>
2019-10-23 11:26:59 -0500

    actualizacion ver_cita

--- ver_cita.php
+++ ver_cita.php
@@ -23,20 +23,17 @@
 </head>
 
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
-  <div id="container">
-  <!--banner-->
-  <div id="container">
-  <?php
+    <!--banner-->
+    <div id="container">
+    <?php
     session_start();
     if(isset($_SESSION['tipousuario'])){
         include("header_index.php");
-  ?>
-  </div>  
-  <?php
-    if(isset($_SESSION['tipousuario'])){
+    ?>
+    </div>  
+    <?php
         //Llamar al archivo MuSQL
         require_once 'Modelo/MySQL.php';
-        
         //Nuevo archivo MySql
         $mysql = new MySQL;
         //Conectar a la base de datos
@@ -54,7 +51,7 @@
             $idUsuario = $_SESSION['idUsuario'];
             //Muestra las citas asignadas a ese usuario 
             $citasUsuario = $mysql->efectuarConsulta("SELECT DATEDIFF(clinica_cotecnova.citas.fecha_hora, DATE_FORMAT(NOW(),'%Y-%m-%d')) as diferencia_dias, clinica_cotecnova.usuarios.nombre_completo as paciente, clinica_cotecnova.medicos.nombre_completo as medico, clinica_cotecnova.citas.motivo_consulta, clinica_cotecnova.citas.fecha_hora from citas join usuarios  on clinica_cotecnova.citas.usuario_id = clinica_cotecnova.usuarios.id_usuario join medicos on clinica_cotecnova.citas.medico_id = clinica_cotecnova.medicos.id_medico where clinica_cotecnova.citas.usuario_id = ".$idUsuario." and  clinica_cotecnova.citas.estado = 1 and clinica_cotecnova.citas.fecha_hora > DATE_FORMAT(NOW(),'%Y-%m-%d') ");     
-    }
+        }
     
     $mysql->desconectar();
         //Si el usuario es medico
@@ -204,8 +201,7 @@
                 </section>
                 <!--/ service-->
             <?php
-        }           
-    }
+        }     
     ?>  
    <!--footer-->
   <div id="footer">
