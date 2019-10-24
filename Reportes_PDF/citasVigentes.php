b756e3a7103625845350b867007347c16b08fbae
CamiloDiza <42385055+CamiloDiza@users.noreply.github.com>
2019-10-24 00:28:55 -0500

    Generar reporte de citas vigentes

--- Reportes_PDF/citasVigentes.php
+++ Reportes_PDF/citasVigentes.php
@@ -1,33 +1,23 @@
 <?php
 
-//Llamar clase PDF y MySQL
+//Llamar clases PDF y MySQL
 require '../Modelo/PDF.php';
 require '../Modelo/MySQL.php';
 
 //Instanciar clases PDF y MySQL
 $pdf = new PDF('L','mm','A4');
-$mysql = new MySQL;
+$mysql = new MySQL();
 
 $mysql->conectar(); //Conectar a BD
 
 //Consultar para traer datos de la tabla citas
-$datos = $mysql->efectuarConsulta("select id_cita, fecha_hora, motivo_consulta, estado from clinica_cotecnova.citas where estado = 1");
-
-$dUsuarios = $mysql->efectuarConsulta("select usuario_id from clinica_cotecnova.citas where estado = 1");
-$dMedicos = $mysql->efectuarConsulta("select medico_id from clinica_cotecnova.citas where estado = 1");
-
-while($resultado = mysqli_fetch_assoc($dUsuarios))
-{
-    $idUsuario = $resultado['usuario_id']; 
-}
-
-while($resultado = mysqli_fetch_assoc($dMedicos))
-{
-    $idMedico = $resultado['medico_id']; 
-}
-
-$usuario = $mysql->efectuarConsulta("select nombre_completo, apellidos from clinica_cotecnova.usuarios where id_usuario = ".$idUsuario."");
-$medico = $mysql->efectuarConsulta("select nombre_completo, apellidos from clinica_cotecnova.medicoswhere id_medico = ".$idMedico."");
+$datos = $mysql->efectuarConsulta("SELECT citas.id_cita, citas.fecha_hora, citas.motivo_consulta, 
+	CONCAT(usuarios.nombre_completo, ' ',usuarios.apellidos) AS 'paciente', 
+	CONCAT(medicos.nombre_completo, ' ',medicos.apellidos) AS 'medico'
+	FROM citas 
+	INNER JOIN usuarios ON usuarios.id_usuario = citas.usuario_id 
+	INNER JOIN medicos ON medicos.id_medico = citas.medico_id 
+	WHERE citas.estado = 1");
 
 $mysql->desconectar(); //Desconectar de BD
 
@@ -36,23 +26,28 @@ $titulo = 'Reporte de citas vigentes';
 $pdf->SetTitle($titulo); 
 
 //Titulos de las columnas
-$header = array('ID','Paciente','Medico','Fecha y hora','Motivo');
-
-//Cargar datos
-//$data = $pdf->LoadData('citasVigentes.txt');
+//$header = array('ID','Paciente','Medico','Fecha y hora','Motivo');
 
 //Generar tabla 
 $pdf->AddPage(); //Añadir pagina al documento
+$pdf->SetFont('Arial','B',14);
+
+//Header
+$pdf->Cell(10,10,'ID',1,0,'C');
+$pdf->Cell(75,10,'Paciente',1,0,'C');
+$pdf->Cell(75,10,'Medico',1,0,'C');
+$pdf->Cell(55,10,'Fecha y hora',1,0,'C');
+$pdf->Cell(60,10,'Motivo',1,1,'C');
+
 $pdf->SetFont('Arial','',14);
-$pdf->BasicTable($header,45,10); //Columnas
 
 while($row = $datos->fetch_assoc())
 {
-    $pdf->Cell(45,10,$row['id_cita'],1,0);
-    $pdf->Cell(45,10,$row[$usuario],1,0);
-    $pdf->Cell(45,10,$row[$medico],1,0);
-    $pdf->Cell(45,10,$row['fecha_hora'],1,0);
-    $pdf->Cell(45,10,$row['motivo_consulta'],1,1);
+    $pdf->Cell(10,10,utf8_decode($row['id_cita']),1,0,'C');
+    $pdf->Cell(75,10,utf8_decode($row['paciente']),1,0);
+    $pdf->Cell(75,10,utf8_decode($row['medico']),1,0);
+    $pdf->Cell(55,10,utf8_decode($row['fecha_hora']),1,0);
+    $pdf->Cell(60,10,utf8_decode( $row['motivo_consulta']),1,1);
 }
 
 $pdf->AliasNbPages(); //Numeracion de paginas
