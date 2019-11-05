1ddfe9d78fcec4d2e2cc12c7bb8d8cbf77875d36
juandiegorb <juan.drb.99@gmail.com>
2019-11-05 16:52:20 -0500

    enviar correo
    Se configura el correo de recuperacion de contraseña local

--- Controlador/enviarCorreo.php
+++ Controlador/enviarCorreo.php
@@ -1,45 +1,40 @@
 <?php
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
-require_once '/../email/src/PHPMailer.php';
-require_once '/../email/src/SMTP.php';
-require_once '/../email/src/Exception.php';
 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../email/src/PHPMailer.php';
+require '../email/src/SMTP.php';
+require '../email/src/Exception.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'pruebacorreocotecnova@gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'pruebacorreocotecnova@gmail.com';                     // SMTP username
     $mail->Password   = 'pruebacotecnova';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->SMTPSecure = 'TLS';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('pruebacorreocotecnova@gmail.com', 'Mailer');
-    $mail->addAddress('juan.drb.99@gmail.com', 'Juan Diego Rios');     // Add a recipient
-    $mail->addAddress('natixflix3@gmail.com'. 'Natalia Agudelo');               // Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
-
-    // Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
+    $mail->setFrom('pruebacorreocotecnova@gmail.com', 'Juan rios');
+    $mail->addAddress('pruebacorreocotecnova@gmail.com');       // Name is optional
+      
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Prueba de correo';
-    $mail->Body    = 'Esto es una prueba de envio de correo';
-    $mail->AltBody = 'Esto es una prueba de correo alternativo';
+    $mail->Subject = 'Recuperar contraseña';
+    $mail->Body    = 'Ingrse al siguiente link para recuperar cambiar la contraseña';
 
     $mail->send();
-    echo 'Message has been sent';
+    echo 'El mensaje se envio correctamente';
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Hubo error al enviar el mensaje: '.  {$mail->ErrorInfo}";
 }
